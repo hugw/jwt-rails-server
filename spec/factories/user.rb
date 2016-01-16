f80a159d0ed6cad24f96feeb1e8cf59b16ff3a0e
Hugo W <hugwnt@gmail.com>
2016-01-16 20:34:55 -0200

    Add user factory

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name                  { Faker::Name.name }
+    email                 { Faker::Internet.email }
+    password              "admin123"
+    password_confirmation "admin123"
+    status                "active"
+  end
+end
