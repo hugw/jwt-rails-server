449046cfff4423036312e28c7b93b23e86a1f02e
Hugo W <hugwnt@gmail.com>
2016-01-16 19:35:47 -0200

    Add Users registration controller

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,29 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :permitted_params
+
+  def create
+    build_resource(sign_up_params)
+
+    resource.status!(:active)
+
+    resource.save
+    if resource.persisted?
+      if resource.active_for_authentication?
+        sign_up(resource_name, resource)
+        respond_with resource, location: nil
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+
+      # Show errors
+      respond_with resource
+    end
+  end
+
+  private
+
+  def permitted_params
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+end
