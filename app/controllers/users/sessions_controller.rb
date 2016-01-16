d5be4f506526a3c7b21d30121da267e42427e7f0
Hugo W <hugwnt@gmail.com>
2016-01-16 19:35:58 -0200

    Add User sessions controller

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,11 @@
+class Users::SessionsController < Devise::SessionsController
+  def create
+    self.resource = warden.authenticate!(auth_options)
+    sign_in(resource_name, resource)
+
+    # Don't need devise auth sessions
+    sign_out()
+
+    render json: resource.jwt, status: :created
+  end
+end
