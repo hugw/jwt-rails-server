acc0b66c7752036efadbe3bf8e38b494ab6c90c9
Hugo W <hugwnt@gmail.com>
2016-01-16 18:42:41 -0200

    Setup root route and default layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+  def root
+  # Setup an entry point to nowhere
+  render :text => nil, :layout => true
+  end
 end
