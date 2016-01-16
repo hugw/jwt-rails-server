59d2ed60f0a83971578444f4d79245a9c594f815
Hugo W <hugwnt@gmail.com>
2016-01-16 19:51:44 -0200

    Add token helper on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,12 @@ class User < ActiveRecord::Base
   def status!(code = :active)
     self.status = STATUS[code]
   end
+
+  def token(raw = nil, ping = 10.minutes.from_now.to_i)
+    # Define what to store
+    # on the token generated
+    user = { id: self.id, ping: ping }
+    token = AuthToken.encode({ user: user })
+    raw.nil? ? { token: token } : token
+  end
 end
