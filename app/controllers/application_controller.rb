c422ed97d20cd31c9579ed687383d94fafeb7bec
Hugo W <hugwnt@gmail.com>
2016-01-16 19:31:18 -0200

    Ensure all app requests are json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  respond_to :json, except: [:root]
+  before_action :json_request?, except: [:root]
   protect_from_forgery with: :null_session
 
   def root
-  # Setup an entry point to nowhere
-  render :text => nil, :layout => true
+    # Setup an entry point to nowhere
+    render :text => nil, :layout => true
+  end
+
+  protected
+
+  def json_request?
+     head :bad_request unless request.format == 'application/json'
   end
 end
