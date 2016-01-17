42091856943aa3f36a6476392059ff890446b172
Hugo W <hugwnt@gmail.com>
2016-01-17 12:41:38 -0200

    Replace DEVISE_KEY with JWT_SECRET

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -2,13 +2,20 @@ require 'jwt'
 
 module AuthToken
   def self.encode(payload)
-    payload['exp'] = 10.days.from_now.to_i
-    JWT.encode(payload, ENV['DEVISE_KEY'])
+    payload['exp'] = 5.days.from_now.to_i
+
+    # Firebase related
+    payload['iat'] = Time.now.to_i
+    payload['v'] = 0
+    uid = "user_" + payload[:user][:id].to_s
+    payload['d'] = { uid: uid }
+
+    JWT.encode(payload, ENV['JWT_SECRET'])
   end
 
   def self.decode(token)
     begin
-      JWT.decode(token, ENV['DEVISE_KEY']).first
+      JWT.decode(token, ENV['JWT_SECRET']).first
     rescue
       false
     end
