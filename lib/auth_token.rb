66c95f15f77776e308282ecb13b76894d6ddfff9
Hugo W <hugwnt@gmail.com>
2016-01-17 00:14:51 -0200

    Update devise keys

--- lib/auth_token.rb
+++ lib/auth_token.rb
@@ -3,12 +3,12 @@ require 'jwt'
 module AuthToken
   def self.encode(payload)
     payload['exp'] = 10.days.from_now.to_i
-    JWT.encode(payload, ENV['devise_key'])
+    JWT.encode(payload, ENV['DEVISE_KEY'])
   end
 
   def self.decode(token)
     begin
-      JWT.decode(token, ENV['devise_key']).first
+      JWT.decode(token, ENV['DEVISE_KEY']).first
     rescue
       false
     end
